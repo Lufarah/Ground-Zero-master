21bbbbbde5bd3a52d16ff5a8820f04ccc5085ccd
Lufarah <lucisalas1105@gmail.com>
2024-01-31 15:26:31 -0300

    prueba 3 Y Examen programacion web

--- home/views.py
+++ home/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.views import View
-
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def home(request):
     context= {}
@@ -27,7 +27,7 @@ def crear(request):
         context= {}
         return render(request, 'home/crear-usuario.html', context)
 
-
+@login_required
 def ticket(request):
     context={}
     return render(request, 'home/ticket.html', context)
